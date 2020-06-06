5c8a175cbe9c92f2cab0d82277fe128dfb6ea22f
Nikita Novik <nikita.a.novik@gmail.com>
2020-06-06 15:48:23 -0700

    Fisrt Commit

---
+++ blogpost.js
@@ -0,0 +1,130 @@
+window.blog = {
+    "blog":
+[
+
+            {   
+            "date":"06-2020",
+            "data":[
+
+                {
+                "number":"001",
+                "date":"06-03-2020",
+                "title":"The Road", 
+                "content": 
+                    [
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+
+                    "<img src='blogposts/06-2020/06-02-2020/001.jpg'>", 
+
+                    "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa.",
+                    
+                    "Aenean venenatis, lorem malesuada eleifend auctor, tellus ex elementum est, non vehicula enim leo nec eros. Pellentesque eu neque ultricies, sagittis leo in, pellentesque quam. Duis at ipsum est. Vivamus molestie elit et dolor euismod lacinia. Vestibulum mollis vestibulum turpis, sit amet varius justo rhoncus id. Ut vel venenatis nunc, ac rhoncus mauris. Pellentesque molestie metus ac leo cursus tristique. Sed sit amet cursus leo.",
+                    
+                    "Etiam fringilla sapien a est eleifend gravida. Curabitur nec mauris nec ligula porttitor vulputate. Nullam congue et ligula ut fringilla. Proin porttitor, ligula gravida accumsan iaculis, libero velit ultrices lectus, a lobortis nunc ipsum sit amet enim. Aliquam eget porttitor leo, et sollicitudin elit. Etiam volutpat arcu neque, non pharetra ante lobortis ac. Integer sit amet tellus at turpis sollicitudin porta vel vel neque. Duis ut mauris sed leo faucibus tempus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vitae eros eu eros lacinia hendrerit vitae a leo."
+                    ]
+                },
+            
+                
+                {
+                "number":"001",
+                "date":"06-02-2020",
+                "title":"Blogpost #2", 
+                "content": "Ipsum Lorem"
+                },
+
+
+                {
+                "title":"3rd blogpost ", 
+                "date":"06-01-2020",
+                "content": 
+                    [
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+
+                    "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa."
+                    ]
+                }
+
+            ]
+         },
+
+        {
+        "date":"05-2020",
+        "data": [
+
+            {
+            "number":"001",
+            "date":"05-17-2020",
+            "title":"The Road", 
+            "content": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+
+            "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa.",
+            
+            "Aenean venenatis, lorem malesuada eleifend auctor, tellus ex elementum est, non vehicula enim leo nec eros. Pellentesque eu neque ultricies, sagittis leo in, pellentesque quam. Duis at ipsum est. Vivamus molestie elit et dolor euismod lacinia. Vestibulum mollis vestibulum turpis, sit amet varius justo rhoncus id. Ut vel venenatis nunc, ac rhoncus mauris. Pellentesque molestie metus ac leo cursus tristique. Sed sit amet cursus leo.",
+            
+            "Etiam fringilla sapien a est eleifend gravida. Curabitur nec mauris nec ligula porttitor vulputate. Nullam congue et ligula ut fringilla. Proin porttitor, ligula gravida accumsan iaculis, libero velit ultrices lectus, a lobortis nunc ipsum sit amet enim. Aliquam eget porttitor leo, et sollicitudin elit. Etiam volutpat arcu neque, non pharetra ante lobortis ac. Integer sit amet tellus at turpis sollicitudin porta vel vel neque. Duis ut mauris sed leo faucibus tempus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vitae eros eu eros lacinia hendrerit vitae a leo."
+            ]},
+        
+            {
+            "number":"001",
+            "date":"05-12-2020",
+            "title":"Blogpost #2", 
+            "content": "Ipsum Lorem"
+            },
+
+
+            {
+            "title":"3rd blogpost ", 
+            "date":"05-01-2020",
+            "content": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+
+            "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa."
+            ]}
+
+                ]
+        },
+
+        {
+            "date":"04-2020",
+            "data": [
+    
+                {
+                "number":"001",
+                "date":"04-17-2020",
+                "title":"Not The Road", 
+                "content": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+    
+                "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa.",
+                
+                "Aenean venenatis, lorem malesuada eleifend auctor, tellus ex elementum est, non vehicula enim leo nec eros. Pellentesque eu neque ultricies, sagittis leo in, pellentesque quam. Duis at ipsum est. Vivamus molestie elit et dolor euismod lacinia. Vestibulum mollis vestibulum turpis, sit amet varius justo rhoncus id. Ut vel venenatis nunc, ac rhoncus mauris. Pellentesque molestie metus ac leo cursus tristique. Sed sit amet cursus leo.",
+                
+                "Etiam fringilla sapien a est eleifend gravida. Curabitur nec mauris nec ligula porttitor vulputate. Nullam congue et ligula ut fringilla. Proin porttitor, ligula gravida accumsan iaculis, libero velit ultrices lectus, a lobortis nunc ipsum sit amet enim. Aliquam eget porttitor leo, et sollicitudin elit. Etiam volutpat arcu neque, non pharetra ante lobortis ac. Integer sit amet tellus at turpis sollicitudin porta vel vel neque. Duis ut mauris sed leo faucibus tempus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vitae eros eu eros lacinia hendrerit vitae a leo."
+                ]},
+            
+                {
+                "number":"001",
+                "date":"05-12-2020",
+                "title":"Not the Blogpost #2", 
+                "content": "Ipsum Lorem"
+                },
+                
+                {
+                    "title":"Another 2nd blogpost ", 
+                    "date":"05-01-2020",
+                    "content": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+        
+                    "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa."
+                    ]},
+    
+                {
+                "title":"Another 3rd blogpost ", 
+                "date":"05-01-2020",
+                "content": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec consequat eros. Suspendisse in dolor volutpat sem varius ultrices dignissim ut dolor. Pellentesque volutpat justo purus, at tincidunt nisl commodo id. Donec cursus rutrum enim et ullamcorper. Nullam dolor mauris, efficitur et vestibulum eget, fermentum a lectus. Quisque non augue finibus, sollicitudin felis sit amet, pulvinar augue. Proin vehicula ante at tortor efficitur, et pretium elit auctor. Sed id ultrices neque. Nam viverra neque quis sem ornare, a efficitur tortor sagittis. Donec interdum viverra lorem, eget bibendum leo luctus in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id neque vitae metus efficitur iaculis.",
+    
+                "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa."
+                ]}
+    
+            ]
+    }
+]
+}
+
