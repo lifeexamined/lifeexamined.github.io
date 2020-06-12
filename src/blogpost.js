d62d5906eacecc9dee7d7ebccdbaadafd9e57441
Nikita Novik <nikita.a.novik@gmail.com>
2020-06-12 15:09:54 -0700

    added picture class

--- src/blogpost.js
+++ src/blogpost.js
@@ -7,10 +7,26 @@ window.blog = {
             "data":[
 
                 {
+                    "title":"Unexamined Life is Not Worth Living", 
+                    "date":"06-01-2020",
+                    "photo":"../blogposts/06-2020/06-05-2020",
+                    "content": 
+                        [
+                        "Unexamined Life is Not Worth Living. These words might seem a bit harsh but it's true. Just like an unread book is not worth keeping or uncooked popcorn is not worth eating, life requires some studying. The examples above seem clear, even ridiculously so, but if we zoom out a little bit, life often resembles that unread book we keep on the shelf (or the uncooked popcorn, for the argument's sake).",
+    
+                        "<a href='../blogposts/06-2020/06-01-2020/original/000.jpg'><img src='../blogposts/06-2020/06-01-2020/000.jpg'>",
+    
+                        "Life is big, it's not just one thing or another, it's not just one book. It's easy to get lost in 'the great library' of life. Examination of life is needed primarily to see what we are made of.", 
+                        
+                        "By studying ourselves we become aware of what we are capable of and make us see what is important (and what is not that important). It allows for efficiency, but more so for happiness by making our lives more colorful."
+                        ]
+                    },
+
+                {
                 "number":"001",
                 "date":"06-05-2020",
                 "title":"The Road",
-                "coverPhoto":"../blogposts/06-2020/06-05-2020/cover-photo.jpg",
+                "photo":"../blogposts/06-2020/06-05-2020",
                 "content": 
                     [
                     "We decided to take a vacation and visit our land in Hartsel, Colorado, a land Kirill bought back in 2017. Since then we haven't had a chance to built anything on it, but the idea has always been very tempting. The location of the land is perfect: it’s a minute away from Hartsel and 30 minutes away from Buena Vista. The land is a dream.",
@@ -24,22 +40,6 @@ window.blog = {
                     "<a href='../blogposts/06-2020/06-05-2020/original/000.jpg'><img src='../blogposts/06-2020/06-05-2020/000.jpg'>", 
 
                     "Quisque ac nisi in nibh iaculis ullamcorper a lacinia felis. Maecenas eu mi aliquet, facilisis erat at, porta odio. Morbi at lacus hendrerit, vestibulum purus quis, sagittis tellus. Nam malesuada ex ac maximus porta. Integer tempus congue urna, eget luctus ex ultrices sit amet. Quisque sagittis rutrum augue, vitae ornare neque facilisis et. Donec mollis diam massa, vitae cursus elit hendrerit a. Etiam id finibus est. In tincidunt, massa et eleifend pretium, nisi ligula iaculis ex, id posuere purus lorem a massa."
-                    ]
-                },
-
-                {
-                "title":"Unexamined Life is Not Worth Living", 
-                "date":"06-01-2020",
-                "coverPhoto":"../blogposts/06-2020/06-01-2020/cover-photo.jpg",
-                "content": 
-                    [
-                    "Unexamined Life is Not Worth Living. These words might seem a bit harsh but it's true. Just like an unread book is not worth keeping or uncooked popcorn is not worth eating, life requires some studying. The examples above seem clear, even ridiculously so, but if we zoom out a little bit, life often resembles that unread book we keep on the shelf (or the uncooked popcorn, for the argument's sake).",
-
-                    "<a href='../blogposts/06-2020/06-01-2020/original/000.jpg'><img src='../blogposts/06-2020/06-01-2020/000.jpg'>",
-
-                    "Life is big, it's not just one thing or another, it's not just one book. It's easy to get lost in 'the great library' of life. Examination of life is needed primarily to see what we are made of.", 
-                    
-                    "By studying ourselves we become aware of what we are capable of and make us see what is important (and what is not that important). It allows for efficiency, but more so for happiness by making our lives more colorful."
                     ]
                 }
 
